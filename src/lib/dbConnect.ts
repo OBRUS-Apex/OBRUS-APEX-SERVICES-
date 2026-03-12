013986fbc25a397e6a1d93f25e0c4d486aa5c58f
ezekielreu6-bit <ezekielreu6@gmail.com>
2026-03-12 16:50:55 +0100

    Update dbConnect.ts

--- src/lib/dbConnect.ts
+++ src/lib/dbConnect.ts
@@ -16,13 +16,24 @@ async function dbConnect() {
   if (cached.conn) {
     return cached.conn;
   }
+
   if (!cached.promise) {
-    const opts = { bufferCommands: false };
+    const opts = {
+      bufferCommands: false,
+    };
+
     cached.promise = mongoose.connect(MONGODB_URI!, opts).then((mongoose) => {
       return mongoose;
     });
   }
-  cached.conn = await cached.promise;
+  
+  try {
+    cached.conn = await cached.promise;
+  } catch (e) {
+    cached.promise = null;
+    throw e;
+  }
+
   return cached.conn;
 }
 
